d1d3970b50638e8fe86d4394e10ce17430c0c7dc
JakubKub13 <jakubkubala3@gmail.com>
2022-11-02 14:44:58 +0100

    Code refactoring

--- contracts/AuctionFactory.sol
+++ contracts/AuctionFactory.sol
@@ -3,21 +3,16 @@ pragma solidity 0.8.17;
 
 import {EnglishAuction} from "./EnglishAuction.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {AuctionToken} from "./AuctionToken.sol";
 
 contract AuctionFactory is Ownable {
     EnglishAuction[] public deployedAuctions;
-    AuctionToken public auctionEther;
     uint256 public ownerFeePool;
     uint256 public immutable creationFee;
     
     event AuctionCreated(address Nft, uint256 NftId, uint256 StartingBid, address Seller);
-    event TokenMinted(address _from, address _to, uint256 _amount);
     event FeeWithdrawal(address to, uint256 amount, uint256 time);
-    event TokenBurnt(address _from, address _to, uint256 _amount);
-
-    constructor(uint256 _creationFee, string memory _name, string memory _symbol) {
-        auctionEther = new AuctionToken(_name, _symbol);
+ 
+    constructor(uint256 _creationFee) {
         creationFee = _creationFee;
     }
 
@@ -27,18 +22,6 @@ contract AuctionFactory is Ownable {
         EnglishAuction newAuction = new EnglishAuction(_nft, _nftId, _startingBid, _seller);
         deployedAuctions.push(newAuction);
         emit AuctionCreated(_nft, _nftId, _startingBid, _seller);
-    }
-
-    function purchaseAuctionTokens() public payable {
-        auctionEther.mint(msg.sender, msg.value); // ration 100 Auction tokens for 1 eth 
-        emit TokenMinted(address(0), msg.sender, msg.value);
-    }
-
-    function burnAuctionTokens(uint256 _amount) public {
-        auctionEther.burnFrom(msg.sender, _amount);
-        (bool success, ) = payable(msg.sender).call{value: _amount}("");
-        require(success, "Auction Factory: Fails burning tokens and returning Ethers");
-        emit TokenBurnt(msg.sender, address(0), _amount);
     }
 
     function ownerFeeWithdraw(address _to, uint256 _amount) public onlyOwner {
