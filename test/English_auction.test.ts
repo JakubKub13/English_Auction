4aebc2705574d1a53c131c46612e2287508a24a9
JakubKub13 <jakubkubala3@gmail.com>
2022-11-03 16:56:09 +0100

    Setting up enviroment for full test

--- test/English_auction.test.ts
+++ test/English_auction.test.ts
@@ -136,9 +136,57 @@ describe("English Auction for tokenized carbon credits", function () {
             await startTx.wait();
             const balance1 = await mDAI.balanceOf(bidder1.address);
             await expect(auctionImplementation.connect(bidder1).bid(ethers.utils.parseEther(bidAmount1.toFixed(18)))).to.be.revertedWith("ERC20: insufficient allowance");
-            const approveTx1 = await mDAI.approve(auctionImplementation.address, balance1);
+            // const approveTx1 = await mDAI.approve(auctionImplementation.address, balance1);
+            // await approveTx1.wait();
+        });
+    });
+
+    describe("Full test after users Bid", function () {
+        beforeEach(async () => {
+            const tx1 = await mDAI.mint(bidder1.address, ethers.utils.parseEther(DAI_AMOUNT_TO_MINT.toFixed(18)));
+            await tx1.wait();
+            const tx2 = await mDAI.mint(bidder2.address, ethers.utils.parseEther(DAI_AMOUNT_TO_MINT.toFixed(18)));
+            await tx2.wait();
+            const tx3 = await mDAI.mint(bidder3.address, ethers.utils.parseEther(DAI_AMOUNT_TO_MINT.toFixed(18)));
+            await tx3.wait();
+            const nftTX = await nft.safeMint(seller.address, "ipfs://carbon_certificate");
+            await nftTX.wait();
+            const auctionCreationTx = await auctionFactory.connect(seller).createAuction(
+                nft.address,
+                0,
+                ethers.utils.parseEther(STARTING_BID.toFixed(18)),
+                seller.address,
+                mDAI.address,
+                {value: ethers.utils.parseEther(FACTORY_FEE_FOR_CREATING_AUCTION.toFixed(18))}
+            )
+            await auctionCreationTx.wait();
+            const addressAuction = await auctionFactory.deployedAuctions(0);
+            const auctionImplementFactory = await ethers.getContractFactory("EnglishAuction");
+            auctionImplementation = auctionImplementFactory.attach(addressAuction);
+
+            const bidder1Amount = "150";
+            const bidder2Amount = "250";
+            const bidder3Amount = "350";
+            const bidder1NewAmount = "550";
+
+            const approveTx = await nft.connect(seller).approve(auctionImplementation.address, 0);
+            await approveTx.wait();
+            const startTx = await auctionImplementation.connect(seller).start(120);
+            await startTx.wait();
+
+            const approveTx1 = await mDAI.connect(bidder1).approve(auctionImplementation.address, ethers.utils.parseEther(bidder1NewAmount));
+            const approveTx2 = await mDAI.connect(bidder2).approve(auctionImplementation.address, ethers.utils.parseEther(bidder2Amount));
+            const approveTx3 = await mDAI.connect(bidder3).approve(auctionImplementation.address, ethers.utils.parseEther(bidder3Amount));
             await approveTx1.wait();
-            await expect(auctionImplementation.connect(bidder1).bid(ethers.utils.parseEther(bidAmount1.toFixed(18)))).to.be.ok;
+            await approveTx2.wait();
+            await approveTx3.wait();
+
+            const bidTx1 = await auctionImplementation.connect(bidder1).bid(ethers.utils.parseEther(bidder1Amount));
+            const bidTx2 = await auctionImplementation.connect(bidder2).bid(ethers.utils.parseEther(bidder2Amount));
+            const bidTx3 = await auctionImplementation.connect(bidder3).bid(ethers.utils.parseEther(bidder3Amount));
+            await bidTx1.wait();
+            await bidTx2.wait();
+            await bidTx3.wait();
         });
     });
 });
