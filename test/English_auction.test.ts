ed90501611b031a56258e194fedf196f6d26018b
JakubKub13 <jakubkubala3@gmail.com>
2022-11-03 11:43:32 +0100

    Deploying mDAI in test

--- test/English_auction.test.ts
+++ test/English_auction.test.ts
@@ -8,13 +8,21 @@ describe("English Auction for tokenized carbon credits", function () {
     let nft: NFTAuction;
     let auctionFactory: AuctionFactory;
     let auctionImplementation: EnglishAuction;
-    let deployer: SignerWithAddress;
+    let owner: SignerWithAddress;
     let seller: SignerWithAddress;
     let bidder1: SignerWithAddress;
     let bidder2: SignerWithAddress;
     let bidder3: SignerWithAddress;
 
     beforeEach(async function() {
-        
+        [owner, seller, bidder1, bidder2, bidder3] = await ethers.getSigners();
+        const mDAIFactory = await ethers.getContractFactory("MockDAI");
+        const nftFactory = await ethers.getContractFactory("NFTAuction");
+        const auctionFactoryFactory = await ethers.getContractFactory("AuctionFactory");
+        const auctionImplementFactory = await ethers.getContractFactory("EnglishAuction");
+        mDAI = await mDAIFactory.deploy();
+        await mDAI.deployed();
+
+
     })
 });
