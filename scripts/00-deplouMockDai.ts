245b2490a5c8d99984774cf1e2a41e612c1dc483
JakubKub13 <jakubkubala3@gmail.com>
2022-11-05 08:40:50 +0100

    Deploy scripts of Mocks

---
+++ scripts/00-deplouMockDai.ts
@@ -0,0 +1,19 @@
+import { ethers, network } from "hardhat";
+import * as dotenv from "dotenv";
+import { MockDAI } from "../typechain-types";
+dotenv.config();
+
+async function main() {
+    let mDai: MockDAI;
+
+    const mDAIFactory = await ethers.getContractFactory("MockDAI");
+    mDai = await mDAIFactory.deploy() as MockDAI;
+    await mDai.deployed();
+
+    console.log(`Mocked DAI was deployed at address of: ${mDai.address}`);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
