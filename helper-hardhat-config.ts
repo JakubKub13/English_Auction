c95322452f4f8e0aaa3ee14423440cf05254bb39
JakubKub13 <jakubkubala3@gmail.com>
2022-11-05 09:26:20 +0100

    Adding helper hardhat file

---
+++ helper-hardhat-config.ts
@@ -0,0 +1,31 @@
+export interface networkConfigItem {
+    name?: string
+    auctionFactory?: string 
+    auctionImplementation?: string
+  }
+export interface networkConfigInfo {
+    [key: number]: networkConfigItem
+}
+
+export const networkConfig: networkConfigInfo = {
+    31337: {
+        name: "localhost",
+        auctionFactory: "",
+        auctionImplementation: "",
+    },
+
+    5: {
+        name: "goerli",
+        auctionFactory: "",
+        auctionImplementation: "",
+    },
+
+    1: {
+        name: "mainnet",
+        auctionFactory: "",
+        auctionImplementation: "",
+    },
+}
+
+export const developmentChains = ["hardhat", "localhost"]
+export const VERIFICATION_BLOCK_CONFIRMATIONS = 6
