990cc2ec5139feff47b7ff98530389d721c0c09c
JakubKub13 <jakubkubala3@gmail.com>
2022-11-05 09:00:22 +0100

    Adding deploy script for Auction Factory

---
+++ scripts/01-deployAuctionFactory.ts
@@ -0,0 +1,22 @@
+import { ethers } from "hardhat";
+import * as dotenv from "dotenv";
+import { AuctionFactory } from "../typechain-types";
+dotenv.config();
+
+async function main() {
+    let auctionFactory: AuctionFactory;
+    const FACTORY_FEE_FOR_CREATING_AUCTION: Number = 0.03;   
+
+    const auctionFactFactory = await ethers.getContractFactory("AuctionFactory");
+    auctionFactory = await auctionFactFactory.deploy(
+        ethers.utils.parseEther(FACTORY_FEE_FOR_CREATING_AUCTION.toFixed(18))
+    );
+    await auctionFactory.deployed();
+
+    console.log(`Auction Factory was deployed at this address: ${auctionFactory.address}`);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
